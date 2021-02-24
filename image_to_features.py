4094b7a7ca8abdbdc81308002661e5463fff7ed9
beefoo <brian@youaremyjoy.org>
2021-02-23 22:02:36 -0500

    update keras import for image to features

--- image_to_features.py
+++ image_to_features.py
@@ -2,6 +2,7 @@
 
 # Adapted from:
 # https://github.com/ml4a/ml4a-guides/blob/master/notebooks/image-search.ipynb
+# https://github.com/ml4a/ml4a-ofx/blob/master/scripts/analyze-images.py
 
 import argparse
 import bz2
@@ -9,6 +10,7 @@ import glob
 import numpy as np
 import os
 import pickle
+from pprint import pprint
 from sklearn.decomposition import PCA
 import sys
 
@@ -17,18 +19,15 @@ from lib.utils import *
 # input
 parser = argparse.ArgumentParser()
 parser.add_argument('-in', dest="INPUT_FILES", default="images/photographic_thumbnails/*.jpg", help="Input file pattern")
-parser.add_argument('-size', dest="IMAGE_SIZE", default="224x224", help="Resize images to this size")
-parser.add_argument('-pca', dest="PCA_COMPONENTS", default=256, type=int, help="Principal component analysis (PCA) components to reduce down to")
+parser.add_argument('-pca', dest="PCA_COMPONENTS", default=300, type=int, help="Principal component analysis (PCA) components to reduce down to")
 parser.add_argument('-out', dest="OUTPUT_FILE", default="output/photographic_features.p.bz2", help="Pickle cache file to store features")
 a = parser.parse_args()
 
-os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
-import keras
-from keras.preprocessing import image
-from keras.applications.imagenet_utils import preprocess_input
-from keras.models import Model
-
-IMAGE_SIZE = tuple([int(d) for d in a.IMAGE_SIZE.strip().split("x")])
+# os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"
+import tensorflow.keras as keras
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.imagenet_utils import preprocess_input
+from tensorflow.keras.models import Model
 
 # Read files
 files = glob.glob(a.INPUT_FILES)
@@ -38,12 +37,16 @@ print("Found %s files" % fileCount)
 
 # Load model, feature extractor
 model = keras.applications.VGG16(weights='imagenet', include_top=True)
+
+targetSize = model.input_shape[1:3]
+print("Target size: %s x %s" % targetSize)
+
 feat_extractor = Model(inputs=model.input, outputs=model.get_layer("fc2").output)
 
 print("Extracting features from each image...")
 features = np.zeros((fileCount, 4096), dtype=np.float32)
 for i, fn in enumerate(files):
-    im = image.load_img(fn, target_size=model.input_shape[1:3])
+    im = image.load_img(fn, target_size=targetSize)
     x = image.img_to_array(im)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
